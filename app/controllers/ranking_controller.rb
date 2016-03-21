d55f54c7372c7621ff7836e0ee117457cf1f6ff4
shin6585 <shinyujiro@gmail.com>
2016-03-21 12:12:12 +0000

    adjust ranking controller

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,10 +1,10 @@
 class RankingController < ApplicationController
     def have
-        @items = Have.group(:item_id).order('count_item_id desc').item
+        @items = Have.group(:item_id).order('count_id desc').count('id')
     end
     
     def want
-        @items = Want.group(:item_id).order('count_item_id desc').item
+        @items = Want.group(:item_id).order('count_id desc').count('id')
     end
     
 end
