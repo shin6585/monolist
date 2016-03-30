1ace55a337f95680cce55786c6310407c3132012
shin6585 <shinyujiro@gmail.com>
2016-03-30 13:34:35 +0000

    adjustcontroller

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,9 @@
 class RankingController < ApplicationController
-    def have
-        @itemkeys = Have.group(:item_id).order('count_id desc').limit(10).count(:id).keys
-    end
-    
-    def want
-        @itemkeys = Want.group(:item_id).order('count_id desc').limit(10).count(:id).keys
-    end
+  def have
+    @items = Have.rankings
+  end
+
+  def want
+    @items = Want.rankings
+  end
 end
--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,9 @@
 class Ownership < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :item, class_name: "Item"
+
+  def self.rankings(limit = 10)
+    ids = self.group(:item_id).order('count_item_id desc').limit(limit).count(:item_id).keys
+    Item.find(ids).sort_by{|o| ids.index(o.id)}
+  end
 end
